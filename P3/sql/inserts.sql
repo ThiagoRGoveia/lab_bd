4d83e89d4e98f1cdc472fc4375b2da09393120c6
Matheus <matheusode@gmail.com>
2023-06-27 17:28:58 -0300

    deleted search_driver_by_name from wrong file

--- P3/sql/inserts.sql
+++ P3/sql/inserts.sql
@@ -27,15 +27,3 @@ EXCEPTION
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE FUNCTION search_driver_by_name(_Forename TEXT, _ConstructorRef VARCHAR)
-RETURNS TABLE(FullName TEXT, DateOfBirth DATE, Nationality VARCHAR) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT d.Forename || ' ' || d.Surname, d.dateofbirth, d.Nationality
-    FROM DRIVER d
-    JOIN RESULTS r ON d.driverid = r.driverid
-    JOIN CONSTRUCTORS c ON r.constructorid = c.constructorid
-    WHERE d.Forename = _Forename AND c.ConstructorRef = _ConstructorRef;
-END;
-$$ LANGUAGE plpgsql;
